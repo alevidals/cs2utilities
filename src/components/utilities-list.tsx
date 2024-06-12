6b29806134583424e85d41cd30c134250e8d98d3
Alvaro <1999alvarito@gmail.com>
2024-06-12 19:57:07 +0200

    chore: props utilities-list

--- src/components/utilities-list.tsx
+++ src/components/utilities-list.tsx
@@ -1,15 +1,13 @@
 import { UtilityItem } from "@/components/utility-item";
 
-type Props = {
-  utilities: { name: string }[];
-};
+type Props = { utilities: string[] };
 
 export function UtilitiesList(props: Props) {
   return (
     <ul className="flex flex-col gap-y-4">
       {props.utilities.map((utility) => (
-        <li key={utility.name}>
-          <UtilityItem name={utility.name} />
+        <li key={utility}>
+          <UtilityItem name={utility} />
         </li>
       ))}
     </ul>
